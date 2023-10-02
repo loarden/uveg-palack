439dc01543a78482ec8d0d6c7d30d8192b78a2a0
loarden <biacsi.ors@gmail.com>
2023-10-02 21:18:35 +0200

    created navbar

---
+++ uveg-palack/src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="container-xl">
+        <ul className="flex-end">
+          <li>Telefon</li>
+          <li>Email</li>
+        </ul>
+      </div>
+      <div className="container-xl">
+        <a className="navbar-brand" href="#">
+          K es B Glass
+        </a>
+        <ul className="navbar-nav">
+          <li className="nav-item"><a href="" className="nav-link">Keydolap</a></li>
+          <li className="nav-item"><a href="" className="nav-link">Keydolap</a></li>
+          <li className="nav-item"><a href="" className="nav-link">Keydolap</a></li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
